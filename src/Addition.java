b747600d160e6614226a667c3e1b01547d7a3b9d
dev <dev@gmail.com>
2023-02-23 20:37:34 +0530

    practice

--- src/Addition.java
+++ src/Addition.java
@@ -2,7 +2,7 @@ public class Addition {
     public static void main(String[] args)
     {
         int a=256;
-        int b=894;
+        int b=596;
         int c=a+b;
         System.out.println(c);
     }
