eb140b3b9a1769cfc7bedf546ba2dc91df8dbf33
SHYAM <shyamburchu@gmail.com>
2023-02-23 20:46:28 +0530

    addition code,return code

--- src/Addition.java
+++ src/Addition.java
@@ -1,7 +1,7 @@
 public class Addition {
     public static void main(String[] args)
     {
-        int a=256;
+        int a=444;
         int b=666;
         int c=a+b;
         System.out.println(c);
