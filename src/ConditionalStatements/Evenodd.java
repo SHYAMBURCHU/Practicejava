a998268b6b8445d4a8bcaa828e2302f3ce0fd3e5
dev <dev@gmail.com>
2023-03-05 19:08:14 +0530

    java input

---
+++ src/ConditionalStatements/Evenodd.java
@@ -0,0 +1,18 @@
+package ConditionalStatements;
+
+public class Evenodd
+{
+    public int evenodd()
+    {
+        int u=123;
+        if(u%2==0)
+        {
+            System.out.println("even");
+        }
+        else
+        {
+            System.out.println("odd");
+        }
+        return u;
+    }
+}
